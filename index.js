637a1c0b9af6b6d7f9bf6403e26b02d3c665567b
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-04-10 00:19:15 +0000

    first commit, final commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (let pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    console.log(`Next pass at ${datetime} for ${pass.duration} seconds.`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
